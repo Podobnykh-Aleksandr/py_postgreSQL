ede9ecfdf12231a0a7b643cc22fb73e92afafb74
Podobnykh-Aleksandr <126334459+Podobnykh-Aleksandr@users.noreply.github.com>
2023-05-12 16:50:34 +0300

    Update insert_req.sql

--- insert_req.sql
+++ insert_req.sql
@@ -1,48 +1,48 @@
-insert into Executor(name, pseudonym)
-values('Юрий Клинских', 'Хой');
-values('Виктор Цой', 'Цой');
-values('Ксения Островская', 'Ангина');
-values('Эдмунд Шклярский', 'Эд Шклярский');
+insert into Executor(name, pseudonym) VALUES
+    ('Юрий Клинских', 'Хой'),
+    ('Виктор Цой', 'Цой'),
+    ('Ксения Островская', 'Ангина'),
+    ('Эдмунд Шклярский', 'Эд Шклярский');
 
-insert into Genre(genre_title)
-values('Рок');
-values('Авторская песня');
-values('Поп');
+insert into Genre(genre_title) VALUES
+    ('Рок'),
+    ('Авторская песня'),
+    ('Поп');
 
-insert into Album(album_title, year)
-values('Весёлый и злой', '2022');
-values('Чужестранец', '2014');
-values('Королевство кривых', '2005');
+insert into Album(album_title, year) VALUES
+    ('Весёлый и злой', '2022'),
+    ('Чужестранец', '2014'),
+    ('Королевство кривых', '2005');
 
-insert into Track(track_title, duration)
-values('Кем бы ты ни был', '03:57');
-values('Чужестранец', '03:56');
-values('Потерянный', '03:54');
-values('Азбука морзе', '04:17');
-values('Мотылёк', '04:12');
-values('Песня эмигранта', '03:09');
+insert into Track(track_title, duration) VALUES
+    ('Кем бы ты ни был', '03:57'),
+    ('Чужестранец', '03:56'),
+    ('Потерянный', '03:54'),
+    ('Азбука морзе', '04:17'),
+    ('Мотылёк', '04:12'),
+    ('Песня эмигранта', '03:09');
 
-insert into Collection(collection_title, release_year)
-values('Классика русского рока', '2022');
-values('Классика русского рока', '2020');
-values('Классика русского рока', '2018');
-values('Классика русского рока', '2016');
+insert into Collection(collection_title, release_year) VALUES
+    ('Классика русского рока', '2022'),
+    ('Классика русского рока', '2020'),
+    ('Классика русского рока', '2018'),
+    ('Классика русского рока', '2016');
 
-insert into GenreExecutor(genre_id, executor_id)
-values('1', '1');
-values('1', '2');
-values('3', '3');
-values('2', '4');
+insert into GenreExecutor(genre_id, executor_id) VALUES
+    ('1', '1'),
+    ('1', '2'),
+    ('3', '3'),
+    ('2', '4');
 
-insert into ExecutorAlbum(executor_id, album_id)
-values('4', '1');
-values('4', '2');
-values('4', '3');
+insert into ExecutorAlbum(executor_id, album_id) VALUES
+    ('4', '1'),
+    ('4', '2'),
+    ('4', '3');
 
-insert into TrackCollection(track_id, collection_id)
-values('1', '1');
-values('2', '1');
-values('3', '1');
-values('4', '1');
-values('5', '1');
-values('6', '1');
+insert into TrackCollection(track_id, collection_id) VALUES
+    ('1', '1'),
+    ('2', '1'),
+    ('3', '1'),
+    ('4', '1'),
+    ('5', '1'),
+    ('6', '1');
