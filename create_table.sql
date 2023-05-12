aa24bd91f64916eae78a842a2d826bbed0d19964
Podobnykh-Aleksandr <126334459+Podobnykh-Aleksandr@users.noreply.github.com>
2023-05-12 16:20:29 +0300

    Update create_table.sql

--- create_table.sql
+++ create_table.sql
@@ -1,47 +1,48 @@
 create table if not exists Track (
-	id SERIAL primary key references Album(id),
+	track_id SERIAL primary key,
+	track_entry BOOL references Album(id), 
 	track_title VARCHAR(60),
 	duration time
 );
 
 create table if not exists Album (
-	id INTEGER primary key references Executor(id),
+	album_id INTEGER primary key,
 	album_title VARCHAR(60),
 	year date 
 );
 
 create table if not exists Executor (
-	id INTEGER primary key references Genre(id),
+	executor_id INTEGER primary key,
 	name VARCHAR(60),
 	pseudonym VARCHAR(60)
 );
 
 create table if not exists Genre (
-	id SERIAL primary key,
+	genre_id SERIAL primary key,
 	genre_title VARCHAR(60) not null
 );
 
 create table if not exists Collection (
-	id INTEGER primary key,
+	collection_id INTEGER primary key,
 	collection_title VARCHAR(60) not null,
 	release_year date
 );
 
 
 create table if not exists GenreExecutor (
-	genre_id INTEGER references Genre(id),
-	executor_id INTEGER references Executor(id),
+	genre_id INTEGER references Genre(genre_id),
+	executor_id INTEGER references Executor(executor_id),
 	constraint pk primary key (genre_id, executor_id)
 );
 
 create table if not exists ExecutorAlbum (
-	executor_id INTEGER references Executor(id),
-	album_id INTEGER references Album(id),
+	executor_id INTEGER references Executor(executor_id),
+	album_id INTEGER references Album(album_id),
 	constraint pk primary key (executor_id, album_id)
 );
 
 create table if not exists TrackCollection (
-	track_id INTEGER references Track(id),
-	collection_id INTEGER references Collection(id),
+	track_id INTEGER references Track(track_id),
+	collection_id INTEGER references Collection(collection_id),
 	constraint pk primary key (track_id, collection_id)
 );
