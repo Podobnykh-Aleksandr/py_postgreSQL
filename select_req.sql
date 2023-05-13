1132aa159e13da0cafd2439e9216c70f2fb4a191
Podobnykh-Aleksandr <126334459+Podobnykh-Aleksandr@users.noreply.github.com>
2023-05-13 22:46:45 +0300

    Update select_req.sql

--- select_req.sql
+++ select_req.sql
@@ -1,44 +1,66 @@
 -- Задание 2
 
 select track_title, duration from Track
-order by duration desc
-where duration(0);
+where duration = (select MAX(duration) from Track);
+
+--
 
 select track_title from Track
-where duration > '00:03:30';
+where duration >= '00:03:30';
+
+--
 
 select collection_title from Collection
 where release_year between 2018 and 2020;
 
+--
+
 select name from Executor
 where name not like '% %';
 
+--
+
 select track_title from Track
-where track_title like '%мой%' and '%my%'; 
+where string_to_array(lower(track_title), ' ') && array['мой', 'my'];  
 
 -- Задание 3
 
-select genre_id, COUNT(executor_id) from GenreExecutor
-group by genre_id;
+select g.genre_title , COUNT(ge.executor_id) from Genre g
+left join GenreExecutor ge on g.genre_id = ge.genre_id 
+group by g.genre_title;
 
-select album.album_id, COUNT(track_id) from Track
-join Album on track_entry = album_id
-where album.year between 2014 and 2023
-group by track_title; 
+-- 
+
+select COUNT(track.track_id) from Track
+join Album on track.track_entry = album.album_id
+where album.year between 2014 and 2023;
+
+--
 
 select album_id, track_id, AVG(duration) from Track
-join Album on track_entry = album_id 
+join Album on track.track_entry = album.album_id 
 group by album_id, track_id 
 order by AVG(duration);
 
-select Executor.executor_id, album.album_id from Executor
-join Album on executor_id = album_id
-where album.year != 2020
-group by executor_title; 
+--
 
-select Collection.collection_id, Executor.executor_id from Collection
-join Executor on collection_id = executor_id
-where Executor.name like '%Шклярский%'
-group by collection_title;
+select executor.name from Executor
+where executor.name not in (
+	select executor.name 
+	from Executor 
+	join ExecutorAlbum on executor.executor_id = executoralbum.executor_id 
+	join Album on executoralbum.album_id = album.album_id
+	where album.year = 2020
+);
+
+-- 
+
+select distinct Collection.collection_id, collection.collection_title from Collection
+join TrackCollection on trackcollection.collection_id = collection.collection_id 
+join Track on track.track_id = trackcollection.track_id
+join Album on track.track_entry = album.album_id
+join ExecutorAlbum on album.album_id = executoralbum.album_id
+join Executor on executoralbum.executor_id = executor.executor_id 
+where Executor.name like '%Шклярский%';
 
 -- 
