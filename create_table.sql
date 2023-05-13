74f321afdd62564e001390ded5326068c0c688a0
Podobnykh-Aleksandr <126334459+Podobnykh-Aleksandr@users.noreply.github.com>
2023-05-13 20:14:11 +0300

    Update create_table.sql

--- create_table.sql
+++ create_table.sql
@@ -1,13 +1,13 @@
-create table if not exists Album (
+create table Album (
 	album_id SERIAL primary key,
 	album_title VARCHAR(60),
-	year INTEGER
+	year INTEGER,
 	track_entry INT
 );
 
 create table if not exists Track (
 	track_id SERIAL primary key,
-	track_entry INT references Album(track_entry), 
+	track_entry INT references Album(album_id), 
 	track_title VARCHAR(60),
 	duration time
 );
@@ -33,17 +33,17 @@ create table if not exists Collection (
 create table if not exists GenreExecutor (
 	genre_id INT references Genre(genre_id),
 	executor_id INT references Executor(executor_id),
-	constraint pk primary key (genre_id, executor_id)
+	constraint pk_ge primary key (genre_id, executor_id)
 );
 
 create table if not exists ExecutorAlbum (
 	executor_id INT references Executor(executor_id),
 	album_id INT references Album(album_id),
-	constraint pk primary key (executor_id, album_id)
+	constraint pk_ea primary key (executor_id, album_id)
 );
 
 create table if not exists TrackCollection (
 	track_id INT references Track(track_id),
 	collection_id INT references Collection(collection_id),
-	constraint pk primary key (track_id, collection_id)
+	constraint pk_tc primary key (track_id, collection_id)
 );
